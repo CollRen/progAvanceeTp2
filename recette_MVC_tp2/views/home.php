21bd97603ae30aef8907c9d4ea7ca04122a82f1a
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-02-25 22:35:50 -0500

    Style

--- recette_MVC_tp2/views/home.php
+++ recette_MVC_tp2/views/home.php
@@ -3,9 +3,13 @@
     <h1>{{ var }}</h1>
 
     <section>
-        
-
-
+            <a href="{{base}}/recette">Recette</a>
+            <a href="{{base}}/auteur">Auteurs</a>
+            <a href="{{base}}/ingredient">Ingrédients</a>
     </section>
+
+    <div>
+        <img src="../public/assets/images/imageMvc_auteurs-copie.jpg" alt="">
+    </div>
 
     {{ include('layouts/footer.php') }}
--- recette_MVC_tp2/views/layouts/header.php
+++ recette_MVC_tp2/views/layouts/header.php
@@ -5,26 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ title }}</title>
     <link rel="stylesheet" href="{{ asset }}/css/style.css">
+    <link rel="stylesheet" href="{{ asset }}/css/main.css">
 </head>
 <body>
     <nav>
         <ul>
             <li><a href="{{base}}/recette">Recette</a></li>
-            <li><a href="{{base}}/recette/create">Creer</a></li>
-            <li>>>></li>
-            <li><a href="{{base}}/auteur">Auteur</a></li>
-            <li><a href="{{base}}/auteur/create">Creer</a></li>
-            <li>||</li>
-            <li><a href="{{base}}/categorie">Catégorie R.</a></li>
-            <li><a href="{{base}}/categorie/create">Creer</a></li>
-            <li>||</li>
-            <li><a href="{{base}}/ingredientCat">Catégorie ING</a></li>
-            <li><a href="{{base}}/ingredientCat/create">Creer</a></li>
-            <li>||</li>
-            <li><a href="{{base}}/ingredient">Ingredient</a></li>
-            <li><a href="{{base}}/ingredient/create">Creer</a></li>
-            <li><a href="">Users</a></li>
-            <li><a href="">Login</a></li>
+            <li><a href="{{base}}/auteur">Auteurs</a></li>
+            <li><a href="{{base}}/ingredient">Ingrédients</a></li>
         </ul>
     </nav>
     <main>
